cb87760af240fefb1ead3e688e1e85e4556c188d
Rafael Zamolo Tanganelli <kabelu@gmail.com>
2025-03-24 20:15:02 +0100

    Config minimal Debian 12

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt install -y gnome-shell gnome-calculator gnome-disk-utility gnome-screenshot gnome-session gnome-shell-extensions gnome-system-monitor gnome-tweaks 
+
+sudo apt install -y nautilus network-manager-gnome network-manager-openvpn network-manager-openvpn-gnome wl-clipboard xsel eog evince cups xorg libxext-dev zlib1g-dev libbsd-dev
+
+sudo apt install -y libfuse3-dev keepassxc firefox-esr git rsync sudo tcpdump gdb clang build-essential python3 python3-pip libftdi1-dev
+
+sudo rm -f /etc/network/interfaces
